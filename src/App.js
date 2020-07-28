a31fd1ae63676ab4e5d830cb50ec7fc28d111b97
ismailasylla <ismailasyllaa@gmail.com>
2020-07-28 16:42:48 +0400

    centered the form on the center && added the logo

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import { Polymath, browserUtils } from '@polymathnetwork/sdk'
 import { Layout, Spin, Form, Input, Button, Divider, Select, Switch, Icon, Typography, Alert, Row, Col, message } from 'antd'
 import useForm from 'rc-form-hooks'
 import { filter } from 'p-iteration'
-import { utils as web3Utils } from 'web3'
+import { utils as web3Utils } from 'web3';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from './images/logo/logo.png';
 
 const { Option } = Select
 const { Content, Header } = Layout
@@ -257,132 +259,137 @@ function App() {
   }
 
   return (
-    <div>
-      <Spin spinning={loading} tip={loadingMessage} size="large">
-        <Layout>
-          <Header style={{
-            backgroundColor: 'white',
-            display: 'flex',
-            flexDirection: 'row',
-            justifyContent: 'flex-end',
-            alignItems: 'center'
-          }}>
-            <Network networkId={networkId} />
-            <User walletAddress={walletAddress} />
-          </Header>
-          <Content style={{
-            padding: 50,
-            backgroundColor: '#FAFDFF'
-          }}>
-            {error && <Alert
-              message={error}
-              type="error"
-              closable
-              showIcon
-            />}
-            <Form colon={false} style={{ maxWidth: 600 }} {...formItemLayout}>
-              <Title level={2} style={{ margin: 25 }}>Reserve Your Token Symbol</Title>
-              <Paragraph style={{ margin: 25 }}>Reservation ensures that no other organization can create a token symbol identical to yours using the Polymath platform. This operation carries a cost of: 250 POLY.</Paragraph>
-              <Item name="symbol"
-                label="Symbol">
-                <Input
-                  placeholder="SYMBOL"
-                  value={formSymbolValue}
-                  onChange={({ target: { value } }) => {
-                    const pattern = RegExp('^[a-zA-Z0-9_-]*$')
-                    if (pattern.test(value) && value.length <= 10) {
-                      setFormSymbolValue(value.toUpperCase())
-                    }
-                  }}
-                />
-              </Item>
-              <Button type="primary" style={{ width: '100%' }} onClick={reserveSymbol}>Reserve Symbol</Button>
-            </Form>
+    <>
+      <div className="d-flex justify-content-end">
+        <Header style={{
+          backgroundColor: 'white',
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'flex-end',
+          alignItems: 'center'
+        }}>
+          <Network networkId={networkId} />
+          <User walletAddress={walletAddress} />
+        </Header>
+      </div>
+      <div className="d-flex justify-content-center">
+        <Spin spinning={loading} tip={loadingMessage} size="large">
 
-            <Divider />
+          <Layout>
+            <Content style={{
+              padding: 50,
+              backgroundColor: '#FAFDFF'
+            }}>
+              {error && <Alert
+                message={error}
+                type="error"
+                closable
+                showIcon
+              />}
+              <Form colon={false} style={{ maxWidth: 600 }} {...formItemLayout}>
+                <img className="logo " src={logo} alt="Logo" />
+                <Title level={2} style={{ margin: 25 }}>Reserve Your Token Symbol</Title>
+                <Paragraph style={{ margin: 25 }}>Reservation ensures that no other organization can create a token symbol identical to yours using the Polymath platform. This operation carries a cost of: 250 POLY.</Paragraph>
+                <Item name="symbol"
+                  label="Symbol">
+                  <Input
+                    placeholder="SYMBOL"
+                    value={formSymbolValue}
+                    onChange={({ target: { value } }) => {
+                      const pattern = RegExp('^[a-zA-Z0-9_-]*$')
+                      if (pattern.test(value) && value.length <= 10) {
+                        setFormSymbolValue(value.toUpperCase())
+                      }
+                    }}
+                  />
+                </Item>
+                <Button className="" type="primary" style={{ width: '100%' }} onClick={reserveSymbol}>Reserve Symbol</Button>
+              </Form>
 
-            {reservations &&
-              <Form colon={false} style={{ maxWidth: 600 }} {...formItemLayout}
-                onSubmit={createToken}>
-                <Title level={2} style={{ margin: 25 }}>Create Your Security Token</Title>
-                <Paragraph style={{ margin: 25 }}>Create your security token using one of your previous symbol reservations. If you let your token reservation expire, the token symbol you selected will be available for others to claim.</Paragraph>
-                <Item
-                  style={{ textAlign: 'left', marginBottom: 25 }}
-                  name="symbol"
-                  label="Reservation">
-                  {getFieldDecorator('symbol', {
-                    rules: [{ required: true, message: 'A token reservation is required' }],
-                  })(<Select
-                    placeholder="Select a reservation">
-                    {reservations.map(({ symbol }) =>
-                      <Option key={symbol} value={symbol}>{symbol}</Option>)}
-                  </Select>)}
-                </Item>
-                <Item
-                  style={{ textAlign: 'left', marginBottom: 25 }}
-                  name="name"
-                  label="Token Name"
-                  extra="This is the name of your token for display purposes. For example: Toro Token">
-                  {getFieldDecorator('name', {
-                    rules: [{ required: true, message: 'Token name is required' }, { max: 64 }],
-                  })(<Input placeholder="Enter Token Name" />)}
-                </Item>
-                <Item
-                  style={{ textAlign: 'left', marginBottom: 25 }}
-                  name="detailsUrl"
-                  label="Token Details"
-                  extra="Paste a link to a web page that includes additional information on your token, such as legend.">
-                  {getFieldDecorator('detailsUrl', { initialValue: '' })(<Input placeholder="Paste link here" />)}
-                </Item>
-                <Item
-                  style={{ textAlign: 'left', marginBottom: 25 }}
-                  name="treasuryWallet"
-                  label="Treasury Wallet Address"
-                  extra="Address of a wallet to be used to store tokens for some operations. Defaults to current user (eg Token Issuer) address">
-                  {getFieldDecorator('treasuryWallet', {
-                    initialValue: walletAddress,
-                    rules: [
-                      { required: true },
-                      {
-                        validator: (rule, value, callback) => {
-                          if (!web3Utils.isAddress(value)) {
-                            callback('Address is invalid')
+              <Divider />
+
+              {reservations &&
+                <Form colon={false} style={{ maxWidth: 600 }} {...formItemLayout}
+                  onSubmit={createToken}>
+                  <Title level={2} style={{ margin: 25 }}>Create Your Security Token</Title>
+                  <Paragraph style={{ margin: 25 }}>Create your security token using one of your previous symbol reservations. If you let your token reservation expire, the token symbol you selected will be available for others to claim.</Paragraph>
+                  <Item
+                    style={{ textAlign: 'left', marginBottom: 25 }}
+                    name="symbol"
+                    label="Reservation">
+                    {getFieldDecorator('symbol', {
+                      rules: [{ required: true, message: 'A token reservation is required' }],
+                    })(<Select
+                      placeholder="Select a reservation">
+                      {reservations.map(({ symbol }) =>
+                        <Option key={symbol} value={symbol}>{symbol}</Option>)}
+                    </Select>)}
+                  </Item>
+                  <Item
+                    style={{ textAlign: 'left', marginBottom: 25 }}
+                    name="name"
+                    label="Token Name"
+                    extra="This is the name of your token for display purposes. For example: Toro Token">
+                    {getFieldDecorator('name', {
+                      rules: [{ required: true, message: 'Token name is required' }, { max: 64 }],
+                    })(<Input placeholder="Enter Token Name" />)}
+                  </Item>
+                  <Item
+                    style={{ textAlign: 'left', marginBottom: 25 }}
+                    name="detailsUrl"
+                    label="Token Details"
+                    extra="Paste a link to a web page that includes additional information on your token, such as legend.">
+                    {getFieldDecorator('detailsUrl', { initialValue: '' })(<Input placeholder="Paste link here" />)}
+                  </Item>
+                  <Item
+                    style={{ textAlign: 'left', marginBottom: 25 }}
+                    name="treasuryWallet"
+                    label="Treasury Wallet Address"
+                    extra="Address of a wallet to be used to store tokens for some operations. Defaults to current user (eg Token Issuer) address">
+                    {getFieldDecorator('treasuryWallet', {
+                      initialValue: walletAddress,
+                      rules: [
+                        { required: true },
+                        {
+                          validator: (rule, value, callback) => {
+                            if (!web3Utils.isAddress(value)) {
+                              callback('Address is invalid')
+                              return
+                            }
+                            callback()
                             return
                           }
-                          callback()
-                          return
                         }
-                      }
-                    ]
-                  })(<Input />)}
-                </Item>
-                <Item
-                  style={{ textAlign: 'left', marginBottom: 25 }}
-                  name="divisible"
-                  label="Divisible"
-                  extra="Indivisible tokens are typically used to represent an equity, while divisible tokens may be used to represent divisible assets such as bonds. Please connect with your advisor to select the best option..">
-                  {getFieldDecorator('divisible', {
-                    initialValue: false,
-                    valuePropName: 'checked',
-                  })(<Switch style={{ float: 'left' }} />)}
-                </Item>
+                      ]
+                    })(<Input />)}
+                  </Item>
+                  <Item
+                    style={{ textAlign: 'left', marginBottom: 25 }}
+                    name="divisible"
+                    label="Divisible"
+                    extra="Indivisible tokens are typically used to represent an equity, while divisible tokens may be used to represent divisible assets such as bonds. Please connect with your advisor to select the best option..">
+                    {getFieldDecorator('divisible', {
+                      initialValue: false,
+                      valuePropName: 'checked',
+                    })(<Switch style={{ float: 'left' }} />)}
+                  </Item>
 
-                <div style={{ width: '100%' }}>
-                  <Row gutter={16}>
-                    <Col span={12}><Button style={{ width: '100%' }} htmlType="reset" onClick={() => resetFields()}>
-                      Reset fields
+                  <div style={{ width: '100%' }}>
+                    <Row gutter={16}>
+                      <Col span={12}><Button style={{ width: '100%' }} htmlType="reset" onClick={() => resetFields()}>
+                        Reset fields
                     </Button></Col>
-                    <Col span={12}> <Button type="primary" style={{ width: '100%' }} htmlType="submit">
-                      Create my token
+                      <Col span={12}> <Button type="primary" style={{ width: '100%' }} htmlType="submit">
+                        Create my token
                     </Button></Col>
-                  </Row>
-                </div>
-              </Form>
-            }
-          </Content>
-        </Layout>
-      </Spin>
-    </div>
+                    </Row>
+                  </div>
+                </Form>
+              }
+            </Content>
+          </Layout>
+        </Spin>
+      </div></>
   )
 }
 
